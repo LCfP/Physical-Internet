b2e607d59bdd83edcf88173e092e282319188200
Sapmetappels <arends_thomas@hotmail.com>
2017-05-23 20:08:53 +0200

    Update Product.java
    Removed the setters.
    Put in the constructor.

--- Product.java
+++ Product.java
@@ -2,74 +2,48 @@ package physicalInternet;
 
 public class Product {
 
-	private double weight = 5.0;
-	private int height = 1;
-	private int width = 1;
-	private int depth = 1;
+	private double weight;
+	private int height;
+	private int width;
+	private int depth;
 	private String productName;
-	private int[][][] dimensionArray = new int[10][10][10];
-	private int dimension = dimensionArray[height][width][depth]; // something goes wrong here
+
+	/**
+	 * Constructor
+	 * 
+	 * @param weight This is the weight of the product.
+	 * @param height This is the height of the product.
+	 * @param width This is the width of the product.
+	 * @param depth This is the depth of the product.
+	 * @param productName This is the name of the product.
+	 */
+	public Product(double weight, int height, int width, int depth, String productName) {
+		this.weight = weight;
+
+		this.height = height;
+		this.width = width;
+		this.depth = depth;
+
+		this.productName = productName;
+	}
 
 	public double getWeight() {
 		return weight;
-	}
-
-	public void setWeight(double weight) {
-		this.weight = weight;
 	}
 
 	public int getHeight() {
 		return height;
 	}
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
 	public int getWidth() {
 		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
 	}
 
 	public int getDepth() {
 		return depth;
 	}
 
-	public void setDepth(int depth) {
-		this.depth = depth;
-	}
-
 	public String getProductName() {
 		return productName;
 	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public int[][][] getDimensionArray() {
-		return dimensionArray;
-	}
-
-	public void setDimensionArray(int[][][] dimensionArray) {
-		this.dimensionArray = dimensionArray;
-	}
-
-	public int getDimension() {
-		return dimension;
-	}
-
-	public void setDimension(int dimension) {
-		this.dimension = dimension;
-	}
-
-	/*
-	 * public Product( double weight, int height, int width, int depth, String
-	 * productName){ this.weight = weight; this.height = height; this.width =
-	 * width; this.depth = depth; this.productName = productName; }
-	 */
-
 }
