601186a64425586503eaf0caf9a5135561720368
Inception7 <j.tent.1@student.rug.nl>
2017-05-26 16:59:30 +0200

    Regional hubs

--- src/physicalInternet/Order.java
+++ src/physicalInternet/Order.java
@@ -1,43 +1,31 @@
-package physicalInternet;
-
-import java.util.ArrayList;
 
 
+/**
+ * @author tom
+ *
+ */
 public class Order {
-	private int customerNumber;
-	private int orderNumber;
+	private  int customerNumber;
+	private  int orderNumber;
 	private String address;
-	private ArrayList<Product> products;
-
 	public int getCustomerNumber() {
-		return this.customerNumber;
+		return customerNumber;
 	}
-
 	public void setCustomerNumber(int customerNumber) {
 		this.customerNumber = customerNumber;
 	}
-
 	public int getOrderNumber() {
-		return this.orderNumber;
+		return orderNumber;
 	}
-
 	public void setOrderNumber(int orderNumber) {
 		this.orderNumber = orderNumber;
 	}
-
 	public String getAddress() {
-		return this.address;
+		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	
 
-	public ArrayList<Product> getProducts() {
-		return this.products;
-	}
-
-	public void setProducts(ArrayList<Product> products) {
-		this.products = products;
-	}
 }
