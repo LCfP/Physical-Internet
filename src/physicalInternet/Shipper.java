f64bd514da610433886e9682351a5559594e752e
Niels Wouda <n.wouda@apium.nl>
2017-05-26 18:09:22 +0200

    More formatting and indentation

--- src/physicalInternet/Shipper.java
+++ src/physicalInternet/Shipper.java
@@ -1,45 +1,31 @@
-
 package physicalInternet;
 
 
 import java.util.ArrayList;
 
 public class Shipper {
+    private ArrayList<Order> orders = new ArrayList<Order>();
 
-	private  ArrayList<Order> orders = new ArrayList<Order>();
-	
+    public void getOrder(Order order) {
+        orders.add(order);
+        showOrdersinShipper();
+    }
 
-	public void getOrder(Order order) {
+    public void showOrdersinShipper() {
+        System.out.println("In Shipper:");
 
-		orders.add(order);
-		showOrdersinShipper();
+        for (Order order : orders) {
+            System.out.println("Address:" + order.getAddress());
+            System.out.println("Customer Number" + order.getCustomerNumber());
+        }
+    }
 
+    public void sendOrderToHub(Order order, Transit transit) {
+        transit.getOrderFromShipper(order);
+        removeOrder(order);
+    }
 
-	}
-
-	public void showOrdersinShipper() {
-		System.out.println("In Shipper:");
-		for (Order order : orders) {
-			System.out.println("Address:" + order.getAddress());
-System.out.println("Customer Number"+order.getCustomerNumber());
-		}
-	}
-
-
-	public void sendOrderToHub(Order order, Transit transit) {
-		transit.getOrderFromShipper(order);
-		removeOrder(order);
-	
-
-	}
-
-	private void removeOrder(Order order) {
-		orders.remove(order);
-
-	}
-
+    private void removeOrder(Order order) {
+        orders.remove(order);
+    }
 }
-
-
-
-
--- src/physicalInternet/Simulation.java
+++ src/physicalInternet/Simulation.java
@@ -1,23 +1,22 @@
 package physicalInternet;
 
+
 public class Simulation {
-	
-	public void runSimulation() {
-		Hub hub = new Hub();
-		 Transit transit =new Transit();
-		
-		for (int time=0;time <10;time++)//no of minutes
-		{
-			Receiver receiver = new Receiver();
-			Shipper shipper = new Shipper();
-			Order order =receiver.placeOrder(time);			
-			receiver.sendOrderToShipper(order, shipper);
-			shipper.sendOrderToHub(order,transit);			
-			transit.transitToHub(time, hub);
-			hub.sendOrderToReceiver(order, transit);
-			transit.transitToReceiver(time, receiver);
-			
-		
-	}
-	
-}}
+
+    public void runSimulation() {
+        Hub hub = new Hub();
+        Transit transit = new Transit();
+
+        for (int time = 0; time < 10; time++) //no of minutes
+        {
+            Receiver receiver = new Receiver();
+            Shipper shipper = new Shipper();
+            Order order = receiver.placeOrder(time);
+            receiver.sendOrderToShipper(order, shipper);
+            shipper.sendOrderToHub(order, transit);
+            transit.transitToHub(time, hub);
+            hub.sendOrderToReceiver(order, transit);
+            transit.transitToReceiver(time, receiver);
+        }
+    }
+}
